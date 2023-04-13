3bf2038abd427f209fd3e95d5e0690cdbc11b53d
Kafiling <kafilkafilth@gmail.com>
2023-04-13 18:59:59 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Navbar'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3c3OqlmASfWZauYxhrUlebUvBvUP_Hzo",
+  authDomain: "prim-chalisa.firebaseapp.com",
+  projectId: "prim-chalisa",
+  storageBucket: "prim-chalisa.appspot.com",
+  messagingSenderId: "284137268292",
+  appId: "1:284137268292:web:30e9911e3ec6fa0e0f0811",
+  measurementId: "G-1YKXQQM7WQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const homeObjOne = {
+  PinkBgColor : false,
+  lightText : true,
+  lightTextDesc : true,
+  topLine : 'เปลี่ยนการเรียนฟิสิกส์แบบเดิมๆ',
+  headline : 'Curious ',
+  description : 'ไขข้อข้องใจต่างๆในรายวิชาฟิสิกส์ด้วยการทดลองเสมือนจัดเตรียมไว้เพื่อคุณ ทำให้คุณได้พบกับมุมมองใหม่ๆและเปลี่ยนการเรียนฟิสิกส์ไปตลอดกาล ',
+  imgStart: '',
+  img : 'https://firebasestorage.googleapis.com/v0/b/keep-curious-v2.appspot.com/o/Homepage%2Fto_the_moon.svg?alt=media&token=c6294016-27a8-445a-bfd0-2eff414494c9',
+  alt : 'Curious'
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar/>
     </div>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+export default function Navbar(){
+
+    return(
+        <div className='Nav-container'>
+            <h1>dsd</h1>
+        </div>
+    )
+}
+
