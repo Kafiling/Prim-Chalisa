eba031c8c760703ad1b91a62053ae84dcafd19d0
Kafiling <kafilkafilth@gmail.com>
2023-05-25 20:50:08 +0700

    animation secceed

--- src/Components/Extra1.jsx
+++ src/Components/Extra1.jsx
@@ -1,56 +1,64 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { InView } from "react-intersection-observer";
 import styles from '../style'
 
 const Extra1 = () => {
+  
    return(
    <>
-      <section id='extra1-1' className={`flex md:flex-row flex-col py-4 md:py-[7.5%] `}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}>
-      <div className="flex flex-row items-center pt-[16px] px-4 mb-2">
-
-        <div className='flex justify-between items-center w-full'>
-          <h4 className='flex-1 font-poppins  ss:text-[32px] text-[24px] text-violet  z-[99]'>
-          Extracurricular Activities
-          </h4>
-        </div>
-        
-      </div>
-      <div className="flex flex-row items-center py-[6px] px-4 mb-2">
-      <div className='flex justify-between items-center w-full'>
-        <h1 className='flex-1 font-poppins font-semibold ss:text-[64px] text-[52px] text-violet ss:leading-[100px] leading-[75px] z-[99] '>
-        HyCare's <br/> Club president 
-          </h1>
-        </div>
-      </div>
-      
-      <p className={`${styles.paragraph} max-w-[470px] mt-5 text-black pb-4 z-[99]`}>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-      <div className='ss:flex hidden'></div>
-      
-    </div>
-    <div className=' flex items-center  justify-center w-[45%]'>
-        <div className="grid grid-cols-2 gap-4 ">
-        <div className="grid gap-4">
+   <InView>
+    {({ inView, ref, entry }) => ( 
+        <section ref={ref} id='extra1-1' className={`flex md:flex-row flex-col py-4 md:py-[7.5%] ` + (inView ? "animate-fade-up animate-ease-in-out animate-delay-300 " : "")}>
+        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}>
+          <div className="flex flex-row items-center pt-[16px] px-4 mb-2">
+    
+            <div className='flex justify-between items-center w-full'>
+              <h4 className={'flex-1 font-poppins  ss:text-[32px] text-[24px] text-violet  z-[99]'}>
+              Extracurricular Activities
+              </h4>
+             
+            </div>
             
-                <img className="h-auto max-w-full rounded-lg animate-fade-up " src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+          </div>
+          <div className="flex flex-row items-center py-[6px] px-4 mb-2">
+          <div className='flex justify-between items-center w-full'>
+            <h1 className='flex-1 font-poppins font-semibold ss:text-[64px] text-[52px] text-violet ss:leading-[100px] leading-[75px] z-[99] '>
+            HyCare's <br/> Club president 
+              </h1>
+            </div>
+          </div>
+          
+          <p className={`${styles.paragraph} max-w-[470px] mt-5 text-black pb-4 z-[99]`}>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+          <div className='ss:flex hidden'></div>
+          
+        </div>
+        <div className=' flex items-center  justify-center w-[45%]'>
+            <div className="grid grid-cols-2 gap-4 ">
+            <div className="grid gap-4">
+                
+                    <img className={"h-auto max-w-full rounded-lg " + (inView ? "animate-fade-up animate-delay-300 animate-ease-in-out" : "")} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+               
+                    <img className={"h-auto max-w-full rounded-lg " + (inView ? "animate-fade-up animate-delay-500 animate-ease-in-out" : "")} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+                
+                    <img className={"h-auto max-w-full rounded-lg " + (inView ? "animate-fade-up animate-delay-700 animate-ease-in-out" : "")} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+                
+            </div>
+            <div className="grid gap-4">
+                
+                    <img className={"h-auto max-w-full rounded-lg " + (inView ? "animate-fade-up animate-delay-[400ms] animate-ease-in-out" : "")} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
+                
+                    <img className={"h-auto max-w-full rounded-lg " + (inView ? "animate-fade-up animate-delay-[600ms] animate-ease-in-out" : "")} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+                
+                    <img className={"h-auto max-w-full rounded-lg " + (inView ? "animate-fade-up animate-delay-[800ms] animate-ease-in-out" : "")} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
+                
+            </div>
+            </div>   
+        </div>
            
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
-            
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
-            
-        </div>
-        <div className="grid gap-4">
-            
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
-            
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
-            
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
-            
-        </div>
-        </div>   
-    </div>
-       
-  </section>
+      </section>
+    )}
+  </InView>
+      
   <p className='text-right pr-[10%] md:pr-0'>01</p>
   <hr className="w-[80%] md:w-[100%] h-[3px] mx-auto border-0 rounded md:my-5 bg-gray-700"></hr>
 
